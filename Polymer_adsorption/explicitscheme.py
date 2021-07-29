352eda9d7ca9cc22d19d378f7cd1263dc400f1c8
timzuntar <timotej.zuntar@gmail.com>
2021-07-29 17:17:04 +0200

    Rudimentary parallelization of most time-intensive process

--- Polymer_adsorption/explicitscheme.py
+++ Polymer_adsorption/explicitscheme.py
@@ -1,7 +1,12 @@
 import math
+import ray
 from cv2 import imread 
 import numpy as np
 from scipy import interpolate,optimize
+
+num_cpus = 3
+ray.init(num_cpus=num_cpus)
+print(f"Initialized ray with {num_cpus} cpus.")
 
 def Timestep(C_current,cmin,dt0):
     cmin_current = C_current.min()
@@ -17,7 +22,7 @@ def RasterToConcentration(C_current,raster,Nx,Ny,c0):
             C_current[i,j] = c0*(raster[j,i]/255.0)
     return C_current
 
-def AdsorptionRateModifier(C_current,C_next,V_current,hz,dt,c0,Rprime,ke,a):
+def AdsorptionRateModifier(C_current,C_next,V_current,Nx,Ny,hz,dt,c0,Rprime,ke,a):
     for i in range(0,Nx):
         for j in range(0,Ny):
             if (i==0): left = c0
@@ -31,7 +36,7 @@ def AdsorptionRateModifier(C_current,C_next,V_current,hz,dt,c0,Rprime,ke,a):
             neighborsum = left + right + forward + backward
             dC = dt*V_current[i,j,0]*Rprime*(c0-C_current[i,j])*(1 + ke*(C_current[i,j] + a*neighborsum)/(1+4*a))
             if (dC/hz >= V_current[i,j,0]):
-                C_next = C_current + V_current[i,j,0]*hz
+                C_next[i,j] = C_current[i,j] + V_current[i,j,0]*hz
                 V_current[i,j,0] = 0.0
                 print("Caution!")
             else:
@@ -65,6 +70,34 @@ def IterateTimeStep(V_current,V_next,D,Nx,Ny,Nz,dt,hlateral,hz,cb,c0):
                     return V_current, pathological
     return V_next, pathological
 
+@ray.remote(num_returns=2)
+def IterateTimeStepParallelised(V_current,D,Nzstart,Nzstop,Nx,Ny,Nz,dt,hlateral,hz,cb,c0):
+    V_next_slice = np.empty((Nx,Ny,Nzstop-Nzstart),dtype="float64")
+    pathological = False
+    for i in range(0,Nx):
+        for j in range(0,Ny):
+            for k in range(Nzstart,Nzstop):
+                if (i==0): left = cb
+                else: left = V_current[i-1,j,k]
+                if (i==Nx-1): right = cb
+                else: right = V_current[i+1,j,k]
+                if (j==0): backward = cb
+                else: backward = V_current[i,j-1,k]
+                if (j==Ny-1): forward = cb
+                else: forward = V_current[i,j+1,k]
+                if (k==Nz-1): bottom = cb
+                else: bottom = V_current[i,j,k+1]
+                if (k==0):
+                    V_next_slice[i,j,k-Nzstart] = V_current[i,j,k] + dt * D * ((left+right+forward+backward - 4*V_current[i,j,k])/(hlateral*hlateral) + (bottom - V_current[i,j,k])/(hz*hz))
+                else:
+                    top = V_current[i,j,k-1]
+                    V_next_slice[i,j,k-Nzstart] = V_current[i,j,k] + dt * D * ((left+right+forward+backward - 4*V_current[i,j,k])/(hlateral*hlateral) + (top+bottom - 2*V_current[i,j,k])/(hz*hz))
+                if(V_next_slice[i,j,k-Nzstart] < 0.0):
+                    print("Negative concentration value encountered! %e -> %e" % (V_current[i,j,k],V_next_slice[i,j,k-Nzstart]))
+                    pathological = True
+                    return V_current, pathological
+    return V_next_slice, pathological
+
 def ExtractROIAverage(C_current,t,simulation_concs,simulation_times,ROI_bounds):
     sum = 0.0
     for i in range(ROI_bounds[0],ROI_bounds[2]+1):
@@ -87,11 +120,10 @@ def CompareSignals(measurement_times,measurement_concs,simulation_times,simulati
     MSD /= N
     return MSD
 
-def SingleRun(x,V_next,C_next,raster,measurement_times,measurement_concs,Nx,Ny,Nz,c0,cb,cmin,hlateral,hz,dt0,ROI_bounds):
+def SingleRun(x,V_next,C_next,raster,measurement_times,measurement_concs,Nx,Ny,Nz,c0,cb,cmin,hlateral,hz,dt0,ROI_bounds,a):
     ke = x[0]
     Rprime = x[1]
-    a = x[2]
-    D = x[3]
+    D = x[2]
     t = 0.0
 
     V_current = np.full((Nx,Ny,Nz),cb)
@@ -103,8 +135,10 @@ def SingleRun(x,V_next,C_next,raster,measurement_times,measurement_concs,Nx,Ny,N
 
     while(C_current.min() <= cmin):
         dt = Timestep(C_current,cmin,dt0)
-        C_next,V_current = AdsorptionRateModifier(C_current,C_next,V_current,hz,dt,c0,Rprime,ke,a)
+        C_next,V_current = AdsorptionRateModifier(C_current,C_next,V_current,Nx,Ny,hz,dt,c0,Rprime,ke,a)
+
         V_next,pathological = IterateTimeStep(V_current,V_next,D,Nx,Ny,Nz,dt,hlateral,hz,cb,c0)
+
         if (pathological == True):
             return 1e6
         print(t,C_current.min())
@@ -115,20 +149,69 @@ def SingleRun(x,V_next,C_next,raster,measurement_times,measurement_concs,Nx,Ny,N
     
     return 1e6*CompareSignals(measurement_times,measurement_concs,simulation_times,simulation_concs,c0)
 
+def SingleRunParallelised(x,V_next,C_next,raster,measurement_times,measurement_concs,Nx,Ny,Nz,c0,cb,cmin,hlateral,hz,dt0,ROI_bounds,a,num_cpus):
+    ke = x[0]
+    Rprime = x[1]
+    D = x[2]
+    t = 0.0
+
+    zslice_height = Nz//num_cpus
+
+    V_current = np.full((Nx,Ny,Nz),cb)
+    C_current = np.empty((Nx,Ny))
+    C_current = RasterToConcentration(C_current,raster,Nx,Ny,c0)
+    simulation_concs = []
+    simulation_times = []
+    ExtractROIAverage(C_current,t,simulation_concs,simulation_times,ROI_bounds)
+
+    while(C_current.min() <= cmin):
+        dt = Timestep(C_current,cmin,dt0)
+        C_next,V_current = AdsorptionRateModifier(C_current,C_next,V_current,Nx,Ny,hz,dt,c0,Rprime,ke,a)
+        timestep = []
+
+        for cpu in range(num_cpus):
+            Nzstart = zslice_height*cpu
+            if (cpu == num_cpus-1):
+                Nzstop = Nz
+            else:
+                Nzstop = zslice_height*(cpu+1)
+            timestep.append(IterateTimeStepParallelised.remote(V_current,D,Nzstart,Nzstop,Nx,Ny,Nz,dt,hlateral,hz,cb,c0))
+
+        for cpu in range(num_cpus):
+            Nzstart = zslice_height*cpu
+            if (cpu == num_cpus-1):
+                Nzstop = Nz
+            else:
+                Nzstop = zslice_height*(cpu+1)
+
+            pathological = ray.get(timestep[cpu])[1]
+            if (pathological == True):
+                return 1e6
+            V_next[:,:,Nzstart:Nzstop] = ray.get(timestep[cpu])[0]  
+
+        print(t,C_current.min())
+        np.copyto(C_current, C_next)
+        np.copyto(V_current, V_next)
+        t += dt
+        ExtractROIAverage(C_current,t,simulation_concs,simulation_times,ROI_bounds)
+    
+    return 1e6*CompareSignals(measurement_times,measurement_concs,simulation_times,simulation_concs,c0)
+
 #The approach is roughly as follows: sample/air interfaces are presented as a 2D grid, the bulk as a 3D grid;
-#each cell has its own adsorbed concentration ranging from 0 to 1 (saturated).
-#A cell which borders cells with high surface concentration is more likely to adsorb additional molecules, so in the event of a rupture the film
-#regrowth is not entirely homogeneous. This is represented by a modified Kisliuk isotherm, also considering neighboring cells with a weighing parameter.
+#each cell has its own adsorbed concentration ranging from 0 to c0 (saturated).
+#A cell's likelihood to adsorb additional molecules is modified by its own surface concentration (and possible that of neighboring cells)
+#so in the event of a rupture the film regrowth is not entirely homogeneous.
+#This is represented by a modified Kisliuk isotherm, also considering neighboring cells with a weighing parameter.
 #Stopping condition for the simulation is concentrations in each surface cell surpassing a specified minimum limit.
 #We define a ROI for space-averaging of data; a single curve is obtained.
 #The curve is then fitted against experimental data. Ideally, the ROIs should be as similar as possible.
-#The model is 4-parametric. If it does not converge, the diffusion constant can be fixed to a realistic value.
+#The model is 3-parametric in practice. The neighbour weighing parameter a can be added as a 4th.
 
 #the average unadsorbed area is a fraction of a millimeter across- one micron resolution should be fine enough to capture details,
 #but short enough that computation time remains bearable
-hlateral = 4e-6 #resolution of ellipsometry images is around 1 micron/pixel. 4x reduced resolution -> 4 microns/pixel
-hz = 4e-6
-c0 = 1e-6 #1 mg/square meter
+hlateral = 4.4e-6 #resolution of ellipsometry images is around 1 micron/pixel. 4x reduced resolution -> 4 microns/pixel
+hz = 4.4e-6
+c0 = 0.81e-6 #1 mg/square meter
 
 dt0 = 0.1
 
@@ -138,10 +221,10 @@ ke = 2e+1
 Rprime = 2e+1
 #a = 0.25
 a = 0.0
-D = 1.1e-11   #rough approximation
+D = 8e-12   #rough approximation
 
-initguess = [ke,Rprime,a,D]
-parameterbounds = ((0.0,None),(0.0,None),(0.0,10.0),(1e-12,1e-10))
+initguess = [ke,Rprime,D]
+parameterbounds = ((0.0,1e2),(0.0,1e2),(5e-12,3e-11))
 
 cb = 3e-2    #boundary concentration (in kg/L)
 cmin = 0.99*c0    #stop iterating when every surface cell surpasses this concentration 
@@ -151,7 +234,7 @@ Ny,Nx = raster.shape
 Nz = int(min(Nx,Ny)*hlateral/hz)
 ROI_bounds = (53,31,59,36)  #ROI1
 #ROI_bounds = (30,29,50,42)    #ROI0
-print("Full size of array is %d, %d, %d" % (Nx,Ny,Nz))
+print("Full size of array is %d, %d, %d. Starting simulation..." % (Nx,Ny,Nz))
 
 out = np.loadtxt("data/exampleinputROI1.txt")
 out = out[np.where(out[range(out.shape[0]),1] < 0.9)]
@@ -159,20 +242,19 @@ measurement_times = out[:,0]
 measurement_concs = out[:,1]
 #measurement points with c > cmin need to be removed, otherwise interpolation will fail
 
-V_next = np.empty((Nx,Ny,Nz))
-C_next = np.empty((Nx,Ny))
+V_next = np.empty((Nx,Ny,Nz),dtype="float64")
+C_next = np.empty((Nx,Ny), dtype="float64")
 
-godobject = (V_next,C_next,raster,measurement_times,measurement_concs,Nx,Ny,Nz,c0,cb,cmin,hlateral,hz,dt0,ROI_bounds)
+godobject = (V_next,C_next,raster,measurement_times,measurement_concs,Nx,Ny,Nz,c0,cb,cmin,hlateral,hz,dt0,ROI_bounds,a,num_cpus)
 
-optimized = optimize.minimize(SingleRun,initguess,bounds=parameterbounds,args=godobject,method="L-BFGS-B",options={"maxfun": 30, "maxiter": 10, "disp": True})
+optimized = optimize.minimize(SingleRunParallelised,initguess,bounds=parameterbounds,args=godobject,method="L-BFGS-B",options={"maxfun": 1, "maxiter": 1, "disp": True})
 print(optimized.x)
 
 ke = optimized.x[0]
 Rprime = optimized.x[1]
-a = optimized.x[2]
-D = optimized.x[3]
+D = optimized.x[2]
 
-cmin = 0.999*c0
+cmin = 0.99*c0
 t = 0.0
 
 V_current = np.full((Nx,Ny,Nz),cb)
@@ -184,9 +266,12 @@ ExtractROIAverage(C_current,t,simulation_concs,simulation_times,ROI_bounds)
 
 while(C_current.min() <= cmin):
     dt = Timestep(C_current,cmin,dt0)
-    C_next,V_current = AdsorptionRateModifier(C_current,C_next,V_current,hz,dt,c0,Rprime,ke,a)
-    V_next = IterateTimeStep(V_current,V_next,D,Nx,Ny,Nz,dt,hlateral,hz,cb,c0)
+    C_next,V_current = AdsorptionRateModifier(C_current,C_next,V_current,Nx,Ny,hz,dt,c0,Rprime,ke,a)
+    V_next,pathological = IterateTimeStep(V_current,V_next,D,Nx,Ny,Nz,dt,hlateral,hz,cb,c0)
     print(t,C_current.min()/c0, V_current.min())
+    if (pathological == True):
+        print("Error!")
+        break
     np.copyto(C_current, C_next)
     np.copyto(V_current, V_next)
     t += dt
@@ -194,9 +279,10 @@ while(C_current.min() <= cmin):
 
 signal = np.array(simulation_concs)
 signal /= c0
-outstring = "data/ROI1_fixed_extratime_%dpar_ke_%.3e_R_%.3e_a_%.3e_D_%.3e_dt0_%.2f.txt" % (len(initguess),ke,Rprime,a,D,dt0)
+outstring = "data/ROI1_optimized_%dpar_ke_%.3e_R_%.3e_a_%.3e_D_%.3e_dt0_%.2f.txt" % (len(initguess),ke,Rprime,a,D,dt0)
 np.savetxt(outstring,np.transpose(np.vstack((simulation_times,np.square(signal)))))
-
+"""
 results = open("data/parameter_output.txt","a")
 results.write("exampleinputROI1 dt %.3f cb %.3e hlateral %.2e c0 %.3e ke %.5e R %.5e a %.5e D %.5e\n" % (dt0,cb,hlateral,c0,ke,Rprime,a,D))
 results.close()
+"""
